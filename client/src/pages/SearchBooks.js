24b62e1b23be7476751c6b4bc58402ef6feac1d9
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2022-04-04 17:44:55 -0400

    updated pages files. Refactored code to use mutations and query

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import {useMutation, useQuery} from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries'
+import { GET_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
